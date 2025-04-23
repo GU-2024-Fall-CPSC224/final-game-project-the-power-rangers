9a3f4ea86f473be61dd54cc76cf31ddad58eb991
KorneevVladislav <vlad.korneev@outlook.com>
2025-04-23 15:56:28 -0700

    Multyplayer

--- src/main/java/edu/gonzaga/Multiplayer.java
+++ src/main/java/edu/gonzaga/Multiplayer.java
@@ -1,6 +1,33 @@
 package edu.gonzaga;
 
+import java.util.ArrayList;
+
 public class Multiplayer
 {
+    private ArrayList<Player> players;
+    private Player currentPlayer;
+    private int match = 0;
 
+    public Multiplayer(){}
+
+    public void AddPlayer(String playerName)
+    {
+        Player newPlayer = new Player(playerName);
+        players.add(newPlayer);
+    }
+
+    public Player GetPlayer(String playerName)
+    {
+        return currentPlayer;
+    }
+
+    public void nextMatch(int loosing_player)
+    {
+        players.remove(match + loosing_player);
+        match += 1;
+        if (match >= players.size())
+        {
+            match = 0;
+        }
+    }
 }
